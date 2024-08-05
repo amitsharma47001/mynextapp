04cdc19e09ff837d99d4d8d05279adb65d0da019
Amit Sharma <amitsharma47001@gmail.com>
2024-08-05 18:57:20 +0545

    changes

--- src/app/api/send/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-
-export async function POST(req, res) {
-  const { email, subject, message } = await req.json();
-  console.log(email, subject, message);
-  
-}
